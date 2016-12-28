7d27eaf1a0da26f279d32f93cd13f63a43f2d8cb
nicolas <nicolas.velasquezgiron@gmail.com>
2016-12-28 09:30:44 -0500

    se edita: Cron_Ejecucion_Modelo.py, este ya ejecuta el modelo en paralelo de manera rapida.

--- 06_Codigos/Cron_Ejecucion_Modelo.py
+++ 06_Codigos/Cron_Ejecucion_Modelo.py
@@ -5,6 +5,8 @@ import os
 import datetime as dt
 import pandas as pd
 import pickle 
+from multiprocessing import Pool
+
 
 #Lectura de cuadales simulados 
 def LeeQsim(ruta):
@@ -20,70 +22,19 @@ def writeQsim(ruta,Q,S):
         pickle.dump(S,f)
         f.close()
 
-
-ruta_ejec = '/home/renea998/scripts/Ejecuta_Modelo_v2.py'
-ruta_cuenca = '/home/renea998/binCuencas/cuencaAMVA_v01.nc'
-ruta_cuenca_temp = '/home/renea998/binCuencas/cuencaAMVA_temp'
-ruta_campo_bin = '/mnt/RADAR/CamposNicolas/Campo_AMVA_operacional3.bin'
-ruta_campo_hdr = '/mnt/RADAR/CamposNicolas/Campo_AMVA_operacional3.hdr'
-ruta_campo_ext_bin = '/mnt/RADAR/CamposNicolas/Campo_AMVA_extrapolated.bin'
-ruta_campo_ext_hdr = '/mnt/RADAR/CamposNicolas/Campo_AMVA_extrapolated.hdr'
-ruta_calibracion = '/home/renea998/Calibraciones/AMVA_01.calib'
-ruta_simulacion = '/home/renea998/Simulaciones/ultimaHora/'
-ruta_historico = '/home/renea998/Simulaciones/'
-ruta_qsim = '/home/renea998/Simulaciones/ultimaHora/Qsim_'
-ruta_qsim_extra = '/home/renea998/Simulaciones/extrapolated/Qsim_ext_'
+rutaEjec = '/home/nicolas/Operacional/Op_Interpolated/06_Codigos/Ejecuta_Modelo_v3.py'
+rutaOper = '/home/nicolas/Operacional/Op_Interpolated/'
+rutaCampos = '/home/nicolas/Operacional/Op_Interpolated/03_Simulaciones/01_Rain/CampoIDW_'
+rutaStorage = '/home/nicolas/Operacional/Op_Interpolated/04_Almacenamiento/'
 
 #-------------------------------------------------------------------
-#OBTIENE LOS REGISTROS DE LAS ULTIMAS 24 HORAS
+#Ejecuta para todos los escenarios
 #-------------------------------------------------------------------
-#Obtiene las posiciones de las ultimas 24 horas
-os.system('tail /mnt/RADAR/CamposNicolas/Campo_AMVA_operacional3.hdr -n 1 > /home/renea998/temporal.txt')
-f = open('/home/renea998/temporal.txt','r')
-Lista = f.readlines()
-f.close()
-#obtiene posicion y cantidad de iteraciones
-Inicio = int(Lista[0].split(',')[0]) - 1
-print Inicio
+Lista = []
+for escena in ['bajo', 'alto']:
+	Lista.append(rutaEjec+' '+rutaOper+' '+rutaCampos+escena+'.bin '+rutaStorage)
+Lista.append(rutaEjec+' '+rutaOper+' '+rutaCampos+'normal.bin '+rutaStorage+' -s')
 
-#-------------------------------------------------------------------
-#Ejecuta
-#-------------------------------------------------------------------
-#Ejecuta un solo intervlao para actualizar condiciones 
-Comando = ruta_ejec+' '+ruta_cuenca+' '+ruta_campo_bin+' '+str(Inicio)+' 1 '+ruta_calibracion+' '+ruta_qsim+' -t 300 -u 0 -s'
-os.system(Comando)
-print Comando
-print 'Ejecuta ultimos 5min'
-
-#-------------------------------------------------------------------
-# ACTUALIZA CAUDALES SIMULADOS E HISTORICOS 
-#-------------------------------------------------------------------
-#Lista caudales 
-Lactual = os.listdir(ruta_simulacion)
-Lactual = [i for i in Lactual if i.endswith('qsim')]
-Lactual.sort()
-Lhisto = os.listdir(ruta_historico)
-Lhisto = [i for i in Lhisto if i.endswith('qsim')]
-Lhisto.sort()
-#Actualiza los caudales historicos 
-for a,h in zip(Lactual, Lhisto):
-	#Lectura de historicos
-	Qhist,Shist = LeeQsim(ruta_historico+h)
-	Qact,Sact = LeeQsim(ruta_simulacion+a)
-	#Acopla actual con historico
-	Q = np.vstack([Qhist.values,Qact.values]).T
-	S = np.hstack([Shist.values.T,Sact.values.T])
-	DatesAct = Qact.index
-	DatesHist = Qhist.index
-	DatesHist = DatesHist.append(DatesAct)
-	QDataFrame = pd.DataFrame(Q[:,1:].T,index = DatesHist[1:],columns = Qhist.columns)
-	SSeries = pd.Series(S[1:],index = DatesHist[1:])
-	#Guarda el resultado
-	writeQsim(ruta_historico+h,QDataFrame,SSeries)
-
-
-#Ejecuta la prediccion
-Comando = ruta_ejec+' '+ruta_cuenca+' '+ruta_campo_ext_bin+' 1 23  '+ruta_calibracion+' '+ruta_qsim_extra+' -t 300 -u 0'
-#print Comando
-os.system(Comando)
-#print 'Ejecuto extrapolacion'
+#Ejecucion
+P = Pool(processes = 3)
+r = P.map(os.system, Lista)

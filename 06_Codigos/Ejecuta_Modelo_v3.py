43de6110454567f95840035f4ebe36df984daa40
nicolas <nicolas.velasquezgiron@gmail.com>
2016-12-28 10:07:30 -0500

    a Ejecuta_Modelo_v3.py se le agrega la posibilidad de guardar los caudales simulados en una ruta especifica.

--- 06_Codigos/Ejecuta_Modelo_v3.py
+++ 06_Codigos/Ejecuta_Modelo_v3.py
@@ -47,8 +47,11 @@ parser=argparse.ArgumentParser(
 parser.add_argument("rutaABS",help="(Obligatorio) Ruta absoluta para buscar en el proyecto operacional donde buscar las cosas .nc")
 parser.add_argument("rutaCampo",help="(Obligatorio) Ruta donde se cuentra el binario con el campo de lluvia (.bin)")
 parser.add_argument("rutaStore",help="(Obligatorio) Ruta donde se guardan las condiciones de almacenamiento de la cuenca")
+parser.add_argument("rutaQsim",help="(Obligatorio) Ruta donde se guardan los cuadlaes simulados")
 parser.add_argument("-v","--verbose",help="(Opcional) Hace que el modelo indique en que porcentaje de ejecucion va",
-        action = 'store_true', default = False)
+	action = 'store_true', default = False)
+parser.add_argument("-s","--store",help="(Opcional) Guarda o no los almacenamientos derivados de la ejecucion",
+	action = 'store_true', default = False)
 
 args=parser.parse_args()
 
@@ -104,48 +107,73 @@ Caudales = {}
 Rain = wmf.read_mean_rain(args.rutaCampo[:-4]+'.hdr', 13, 0)
 #Ejecucion del modelo
 for k in Proyectos:
-    # Se fija que el proyecto tenga calibracion
-    if Proyectos[k]['calibValues'] <> None:
-        #Si si, comienza a cargar las cosas para la ejecuicion
-        cu = wmf.SimuBasin(0,0,0,0, rute=Proyectos[k]['cuenca'])
-        Caudales.update({k:{}})
-        posControl = wmf.models.control[wmf.models.control<>0]
-        #Itera para cada almacenamiento
-        for k2 in Proyectos[k]['calibValues'].keys():
-            #Obtiene la calibracion
-            Calib = Proyectos[k]['calibValues'][k2]
-            #Obtiene el almacenamiento 
-            try:
-                #Mira si hay almacenamiento para esa calibracion
-                p = Proyectos[k]['storeFile'].keys().index(k2)
-                # obtiene las rutas del binario y del encabezado de almacenamiento 
-                ruta_sto_bin, ruta_sto_hdr = wmf.__Add_hdr_bin_2route__(Proyectos[k]['storeFile'][k2],
-                    storage = True)
-                # Carga las condiciones en la cuenca
-                cu.set_Storage(ruta_sto_bin,0)
-                #Corre la cuenca 
-                Results = cu.run_shia(Calib, args.rutaCampo, 13, 1,
-                    ruta_storage = Proyectos[k]['storeFile'][k2],
-                    kinematicN = 7)
-                print 'Aviso: El proyecto '+k+' corrio con la calibracion: '+k2 
-            except:
-                print 'Aviso: El proyecto '+k+' en la calibracion '+k2+' no cuenta con almacenamiento, se establece alm inicial en 0.0'
-                #corre y guarda condiciones al 
-                for i in range(5):
-                    cu.set_Storage(0,i)
-                Results = cu.run_shia(Calib, args.rutaCampo, 13, 1,
-                    ruta_storage = args.rutaStore + 'Cuen999_'+k+'_'+k2,
-                    kinematicN = 7)
-            
-            #Copia caudales             
-            Qsim = pnd.DataFrame(Results['Qsim'][1:].T,
-                index=Rain.index,
-                columns=posControl)
-            Caudales[k].update({k2:Qsim})
-    else:
-        print 'Error: El proyecto '+k+' no cuenta con ningun tipo de calibracion'
+	# Se fija que el proyecto tenga calibracion
+	if Proyectos[k]['calibValues'] <> None:
+		#Si si, comienza a cargar las cosas para la ejecuicion
+		cu = wmf.SimuBasin(0,0,0,0, rute=Proyectos[k]['cuenca'])
+		Caudales.update({k:{}})
+		posControl = wmf.models.control[wmf.models.control<>0]
+		#Itera para cada almacenamiento
+		for k2 in Proyectos[k]['calibValues'].keys():
+			#Obtiene la calibracion
+			Calib = Proyectos[k]['calibValues'][k2]
+			#Obtiene el almacenamiento 
+			try:
+				#Mira si hay almacenamiento para esa calibracion
+				p = Proyectos[k]['storeFile'].keys().index(k2)
+				# obtiene las rutas del binario y del encabezado de almacenamiento 
+				ruta_sto_bin, ruta_sto_hdr = wmf.__Add_hdr_bin_2route__(Proyectos[k]['storeFile'][k2],
+					storage = True)
+				# Carga las condiciones en la cuenca
+				cu.set_Storage(ruta_sto_bin,0)
+				#Corre la cuenca 
+				if args.store: 
+					Guarda = Proyectos[k]['storeFile'][k2]
+					print 'Aviso!: Actualiza condiciones'
+				else:
+					Guarda = None
+					print 'Aviso!: No actualiza condiciones'
+				#Ejecuta
+				Results = cu.run_shia(Calib, args.rutaCampo, 13, 1,
+					ruta_storage = Guarda,
+					kinematicN = 7)
+				print 'Aviso: El proyecto '+k+' corrio con la calibracion: '+k2 
+			except:
+				print 'Aviso: El proyecto '+k+' en la calibracion '+k2+' no cuenta con almacenamiento, se establece alm inicial en 0.0'
+				#condiciones iniciales en cero
+				for i in range(5):
+					cu.set_Storage(0,i)
+				#Guarda no guarda
+				if args.store: 
+					Guarda = args.rutaStore + 'Storage_'+k+'_'+k2
+					print 'Aviso!: Actualiza condiciones'
+				else:
+					Guarda = None
+					print 'Aviso!: No actualiza condiciones'
+				#Ejecuta
+				Results = cu.run_shia(Calib, args.rutaCampo, 13, 1,
+					ruta_storage = Guarda,
+					kinematicN = 7)
+			
+			#Copia caudales             
+			Qsim = pnd.DataFrame(Results['Qsim'][1:].T,
+				index=Rain.index,
+				columns=posControl)
+			Caudales[k].update({k2:Qsim})
+	else:
+		print 'Error: El proyecto '+k+' no cuenta con ningun tipo de calibracion'
 
 #-------------------------------------------------------------------
 #Guarda los caudales 
 #-------------------------------------------------------------------
-	
+print 'Aviso: Ingresa a grabar caudales simulados'
+
+for k in Caudales.keys():
+    for k2 in Caudales[k].keys():
+        #Guarda el archivo 
+        nombre = args.rutaQsim + 'Qsim_'+k+'_'+k2+'.qsim'
+        f = open(nombre, 'w')
+        pickle.dump(Caudales[k][k2], f)
+        f.close()
+        print 'Se guarda: '+nombre
+

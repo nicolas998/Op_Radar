bdebe60350de0e6e7bc3f1dc8d5cc1ab498b2551
nicolas <nicolas.velasquezgiron@gmail.com>
2017-01-06 17:27:53 -0500

    1. Se genera el cron: Cron_Actualiza_Obs.py el cual actualizara y generara historico de caudales observados con los cuales se realizaran figuras y comparaciones para la modelacion

---
+++ 06_Codigos/Cron_Actualiza_Obs.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import numpy as np 
+import pickle 
+import pandas as pnd
+import os 
+import datetime as dt
+
+#-------------------------------------------------------------------
+#VARIABLES DEL CRON
+#-------------------------------------------------------------------
+rutaEst = '/home/nicolas/Operacional/Op_Interpolated/03_Simulaciones/03_Stream_Observed/Comparison_Stations.txt'
+rutaFile = '/home/nicolas/Operacional/Op_Interpolated/03_Simulaciones/03_Stream_Observed/'
+rutaTemp = '/home/nicolas/Operacional/Op_Interpolated/03_Simulaciones/03_Stream_Observed/Qobs_temp.qobs'
+# replace: indica si se va a remplazar toda la serie historica consultada de caudales o no, por defecto se deja en que no 
+replace = False
+# DeltaAtras: Cantidad de pasos en minutos hacia atras para que se realice la consulta 
+DeltaAtras = 10
+
+#-------------------------------------------------------------------
+#LEE PARAM DE LOS CAUDALES OBSERVADOS QUE DEBE CONSULTAR
+#-------------------------------------------------------------------
+#Lectura del archivo con la informacion de las estaciones con caudales.
+f = open(args.rutaEst,'r')
+L = f.readlines()
+f.close()
+#Obtiene en un diccionario las caracteristicas de las estaciones 
+DictCaudal = {}
+for j in L[7:]:
+    Data = j.split(',')
+    DictCaudal.update({Data[0]:{}})
+    DictCaudal[Data[0]].update({'ID':int(Data[1])})
+    DictCaudal[Data[0]].update({'Nodo':int(Data[2])})
+    DictCaudal[Data[0]].update({'Coef':float(Data[3])})
+    DictCaudal[Data[0]].update({'Expo':float(Data[4])})
+
+#-------------------------------------------------------------------
+#Fechas para la consulta 
+#-------------------------------------------------------------------
+# Obtiene el datetime 
+fecha_1 =  dt.datetime.now() - dt.timedelta(minutes = 10)
+#Si no es cercano a 5 lo hace igual a un numero multiplo de cinco
+if fecha_1.minute%5 <> 0:
+    minute = int(np.floor(fecha_1.minute/10.0) * 10)
+    fecha_1 = dt.datetime(fecha_1.year, fecha_1.month, fecha_1.day, fecha_1.hour, minute)
+fecha_2 =  dt.datetime.now()# - dt.timedelta(minutes = 50)
+if fecha_2.minute%5 <> 0:
+    minute = int(np.floor(fecha_2.minute/10.0) * 10)
+    fecha_2 = dt.datetime(fecha_2.year, fecha_2.month, fecha_2.day, fecha_2.hour, minute)
+# Lo convierte en texto 
+fecha1 = fecha_1.strftime('%Y-%m-%d')
+fecha2 = fecha_2.strftime('%Y-%m-%d')
+hora_1 = fecha_1.strftime('%H:%M')
+hora_2 = fecha_2.strftime('%H:%M')
+
+#-------------------------------------------------------------------
+#Consulta y mira si debe actualizar
+#-------------------------------------------------------------------
+#Lista de caudales observados historicos acutales 
+L = os.listdir(rutaFile)
+L = [i for i in L if i.endswith('.qobs')]
+
+#Itera sobre las estaciones que tienen caudales para consultarlas 
+for k in DictCaudal.keys():
+    #Para consultar una estacion
+    idEst = str(DictCaudal[k]['ID'])
+    c = str(DictCaudal[k]['Coef'])
+    e = str(DictCaudal[k]['Expo'])
+    #Nombre historico caudal 
+    nombre = 'Qobs_'+k+'.qobs'
+    # Mira si ya existen registros en la carpeta para esa estacion 
+    Existe = False
+    try:
+        pos = L.index(nombre)
+        Existe = True
+        if replace:
+            rutaFin = rutaFile + nombre
+        else:
+            rutaFin = rutaTemp
+    except:
+        rutaFin = rutaFile + nombre
+    print rutaFin
+    #Genera el comando de consulta y lo ejecuta
+    comando = ('/home/nicolas/Operacional/Op_Interpolated/06_Codigos/Consulta_Caudal.py '+fecha1+' '+fecha2+' '
+        +rutaFin+' '+idEst+' -t 5min -1 '+hora_1+' -2 '+hora_2
+        +' -c '+c+' -e '+e)
+    os.system(comando)
+    #Si ya existe le adjunta la nueva consulta 
+    if Existe:
+        Qhist = pnd.read_msgpack(rutaFile + L[pos])
+        Qactual = pnd.read_msgpack(rutaTemp)
+        #Junta ambos resgistros
+        D = pnd.date_range(Qhist.index[0], Qactual.index[-1], freq='5min')
+        Qjoin = pnd.Series(np.zeros(D.shape), D)
+        Qjoin[Qhist.index] = Qhist
+        Qjoin[Qactual.index] = Qactual
+        #Guarda el archivo historico 
+        Qjoin.to_msgpack(rutaFile + L[pos])
+    print 'consultado: '+k
+
